ac7d33d2e477a77e25a3a18e03392fd42313fffa
Armend Daci <armenddaci@pop-os.localdomain>
2023-02-15 15:32:09 -0500

    First commit

---
+++ bank/accounts.py
@@ -0,0 +1,16 @@
+class Bank:
+    pass
+
+
+class Checking:
+    pass
+
+
+class Savings:
+    pass
+
+
+class CustomerAccount:
+    def __init__(self):
+        checking = Checking()
+        savings = Savings()
