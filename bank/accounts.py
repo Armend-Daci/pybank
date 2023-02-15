1c0ef53c87eff1c105d177b907d989c556e2b077
Armend Daci <armenddaci@pop-os.localdomain>
2023-02-15 15:59:11 -0500

    Updated Bank init function

--- bank/accounts.py
+++ bank/accounts.py
@@ -1,13 +1,16 @@
-class Bank:
-    pass
-
-
 class Checking:
     pass
 
 
 class Savings:
     pass
+
+
+class Bank:
+    def __init__(self, accountnum, checking, savings):
+        self.accountnum = accountnum
+        self.checking = checking
+        self.savings = savings
 
 
 class CustomerAccount:
