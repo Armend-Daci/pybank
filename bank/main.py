126da37a465614aac178e369920e9fee25e4a81a
Armend Daci <armenddaci@pop-os.localdomain>
2023-02-15 19:58:53 -0500

    Updated Account Creator Function

--- bank/main.py
+++ bank/main.py
@@ -7,19 +7,19 @@ def initialize():
     customer1 = ("Armend", "Daci", "1", bank1)
 
     check2 = Checking(6000)
-    bank2 = Bank(10000, check2, None)
+    bank2 = Bank(10001, check2, None)
     customer2 = ("Mersim", "Daci", "1", bank2)
 
     check3 = Checking(1000)
-    bank3 = Bank(10000, check3, None)
+    bank3 = Bank(10002, check3, None)
     customer3 = ("John", "Wick", "1", bank3)
 
     check4 = Checking(84000)
-    bank4 = Bank(10000, check4, None)
+    bank4 = Bank(10003, check4, None)
     customer4 = ("Arny", "Schwartz", "1", bank4)
 
     check5 = Checking(4700)
-    bank5 = Bank(10000, check5, None)
+    bank5 = Bank(10005, check5, None)
     customer5 = ("Melvin", "Donovan", "1", bank5)
 
     list.append(customer1)
@@ -163,6 +163,7 @@ def initialmenu():
                 main()
                 """
 
+
 def accountcreator(list, accounttype):
     first = 10000
     accounts = []
@@ -173,9 +174,7 @@ def accountcreator(list, accounttype):
         print(key)
         if key == 0:
             start = values[3].getAccount()
-        # print(key, values)
         accounts.append(values[3].getAccount())
-        # print(accounts)
 
     numbers = []
     for key, values in enumerate(accounts):
@@ -196,9 +195,6 @@ def accountcreator(list, accounttype):
         print(first)
         return first
 
-    num = 10000
-
-    return num
 
 if __name__ == '__main__':
     list = initialize()
